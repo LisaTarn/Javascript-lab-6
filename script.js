b2c6ffbdc686fb19f3b82a2f690df69d94909801
LisaTarn <150932484+LisaTarn@users.noreply.github.com>
2024-11-08 12:55:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
-async function fetchUserProfiles (){
-  try {
-    const response = await fetch ('/userprofiles');
-    if (!response.ok){
-      throw new Error('Failed to fetch user profiles');
-    }
-    const userprofiles = response.json();
-    return userprofiles;
-} catch (error){
-  console.error('Failed to fetch user profiles');
-}};
+//asynchronous function to simulate data fetching for userprofiles
 
-async function fetchPosts () {
-  try {
-    const response = await fetch ('/posts');
-    if (!response.ok){
-      throw new Error('Failed to fetch posts');
-    }
-    const posts = response.json();
-    return posts;
-  } catch (error){
-    console.error('Failed to fetch posts');
-  }
-};
-
-async function fetchComments () {
-  try {
-    const response = await fetch ('/comments');
-  if (!response.ok){
-    throw new Error('Failed to fetch comments');
-  }  
-    const comments = response.json();
-    return comments;
-} catch (error){
-  console.error('Failed to fetch comments');
+async function fetchUserProfiles (){ 
+  const response = await fetch ('/userprofiles');
+  const userprofiles = response.json
+  console.log(userprofiles)
 }
-};
+
+//asynchronous function to simulate data fetching for posts
+
+async function fetchPosts (){ 
+  const response = await fetch ('/posts');
+  const posts = response.json
+  console.log(posts)
+}
+
+//asynchronous function to simulate data fetching for comments
+
+async function fetchComments (){ 
+  const response = await fetch ('/comments');
+  const comments = response.json
+  console.log(comments)
+}
+
+//Sequential data fetching using then 
 
 let promise = new Promise ((resolve, reject) => {resolve();
 
@@ -48,9 +36,13 @@ promise.then(result => {
       result = fetch ('/comments');
       return result;
     });
+ 
+ //Parallel data fetching
 
-Promise.all ([fetchUserProfiles, fetchPosts, fetchComments]).then((values) => {console.log(values)});
+ Promise.all ([fetchUserProfiles, fetchPosts, fetchComments]).then((values) => {console.log(values)});
 
+ //Refactoring with await
+ 
 async function sequentialFetching () {
   try {
   const response1 = await fetch ('/userprofiles');
@@ -74,10 +66,52 @@ async function sequentialFetching () {
     const comments = response3.json();
     console.log(comments);}
     catch (error){
-      console.error("Could not get the information: ${error}");
+      console.log("Could not get the information: ${error}");
       throw error;
     }
 }
+
+// Modifying functions with error handling (try and catch)
+
+async function fetchUserProfiles (){
+  try {
+    const response = await fetch ('/userprofiles');
+    if (!response.ok){
+      throw new Error('Failed to fetch user profiles');
+    }
+    const userprofiles = response.json();
+    return userprofiles;
+} catch (error){
+  console.log('Failed to fetch user profiles');
+}};
+
+async function fetchPosts () {
+  try {
+    const response = await fetch ('/posts');
+    if (!response.ok){
+      throw new Error('Failed to fetch posts');
+    }
+    const posts = response.json();
+    return posts;
+  } catch (error){
+    console.log('Failed to fetch posts');
+  }
+};
+
+async function fetchComments () {
+  try {
+    const response = await fetch ('/comments');
+  if (!response.ok){
+    throw new Error('Failed to fetch comments');
+  }  
+    const comments = response.json();
+    return comments;
+} catch (error){
+  console.log('Failed to fetch comments');
+}
+};
+
+// Chaining the async functions together
 
 function getUserContent(){
 
@@ -89,4 +123,5 @@ function getUserContent(){
   console.log("Comments retrieved");
 
 }
+
 getUserContent;
